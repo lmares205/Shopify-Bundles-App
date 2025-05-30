d4446e8e9aaa970c33936d0551bf5621c900807a
Leslie Mares <leslie@Leslies-MacBook-Pro.local>
2025-05-29 20:05:46 -0700

    create new fixed bundle page

---
+++ app/routes/app.fixedBundle.tsx
@@ -0,0 +1,19 @@
+import {
+    Box,
+    Card,
+    Layout,
+    Link,
+    List,
+    Page,
+    Text,
+    BlockStack,
+  } from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+export default function FixedBundlePage() {
+    return (
+        <Page>
+            <TitleBar title="Fixed Bundle" />
+        </Page>
+    );
+}
