ffa042a471bd3bb9f4360c9b063161a87df2a555
Leslie Mares <leslie@Leslies-MacBook-Pro.local>
2025-05-29 20:59:05 -0700

    add product picker and button

--- app/routes/app.fixedBundle.tsx
+++ app/routes/app.fixedBundle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
     Box,
     Card,
@@ -7,13 +8,34 @@ import {
     Page,
     Text,
     BlockStack,
+    Button,
   } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import { Product } from "@shopify/app-bridge-types";
 
 export default function FixedBundlePage() {
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    // https://shopify.dev/docs/api/app-bridge-library/apis/resource-picker
+    async function selectProduct() {
+        const selectedProducts = await shopify.resourcePicker({
+            type: "product",
+            multiple: true
+        }) as Product[] | undefined;
+
+        if (selectedProducts) {
+            setProducts(products.concat(selectedProducts));
+        }
+    }
+
     return (
         <Page>
             <TitleBar title="Fixed Bundle" />
+
+            <Button onClick={selectProduct} id="select-product">
+                Select product
+            </Button>
+
         </Page>
     );
 }
